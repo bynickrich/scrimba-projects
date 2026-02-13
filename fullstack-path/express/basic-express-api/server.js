c8b2189b54f8eade8e0807730235bf66386d0c7d
Nick Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2026-02-12 22:03:41 -0500

    Start of Express API Section.

---
+++ fullstack-path/express/basic-express-api/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const PORT = 8000;
+
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham",
+};
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json(celebrity);
+});
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
