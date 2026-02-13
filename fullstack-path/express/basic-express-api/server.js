d892a96295b9e095e54e0e3385512eca439709b8
Nick Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2026-02-12 22:07:46 -0500

    Lesson complete.

--- fullstack-path/express/basic-express-api/server.js
+++ fullstack-path/express/basic-express-api/server.js
@@ -1,16 +1,12 @@
 import express from "express";
+import { startups } from "./data/data";
 
 const PORT = 8000;
-
-const celebrity = {
-  type: "action hero",
-  name: "JSON Statham",
-};
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json(celebrity);
+  res.json(startups);
 });
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
