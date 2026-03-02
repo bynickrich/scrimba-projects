45a3ba7dd869f0f3e2b118dd707d1a66b814eded
Nick.Richardson <90582249+nicjrichDEV@users.noreply.github.com>
2026-03-01 21:39:26 -0500

    more practice

--- fullstack-path/testing/meme-generator/src/components/Main.test.jsx
+++ fullstack-path/testing/meme-generator/src/components/Main.test.jsx
@@ -1,10 +1,36 @@
 import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import Main from "./Main";
 
-test("Test default text and image rendering", () => {
-  render(<Main />);
-  expect(screen.getByRole("img").src).toBe("https://i.imgflip.com/1bij.jpg");
-  expect(screen.getByText("One does not simply")).toBeInTheDocument();
-  expect(screen.getByText("Walk into Mordor")).toBeInTheDocument();
+describe("Main component", () => {
+  test("Test default text and image rendering", () => {
+    render(<Main />);
+    expect(screen.getByRole("img").src).toBe("https://i.imgflip.com/1bij.jpg");
+    expect(screen.getByText("One does not simply")).toBeInTheDocument();
+    expect(screen.getByText("Walk into Mordor")).toBeInTheDocument();
+  });
+
+  // Labels
+  test("Input labels render", () => {
+    render(<Main />);
+    expect(screen.getByText("Top Text")).toBeInTheDocument();
+    expect(screen.getByText("Bottom Text")).toBeInTheDocument();
+  });
+
+  // Input fields placeholders
+  test("Inputs have placeholder text", () => {
+    render(<Main />);
+    expect(
+      screen.getByPlaceholderText("One does not simply"),
+    ).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Walk into Mordor")).toBeInTheDocument();
+  });
+
+  // Button with default text
+  test("Button", () => {
+    render(<Main />);
+    expect(screen.getByRole("button")).toHaveTextContent(
+      "Get a new meme image 🖼",
+    );
+  });
 });
