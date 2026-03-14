d2229eab2768f90f24dca863e3cccd8b6a9da2a4
Nick.Richardson <90582249+bynickrich@users.noreply.github.com>
2026-03-13 21:16:19 -0400

    continued advancement

--- fullstack-path/advanced-react/src/context/AuthContext.jsx
+++ fullstack-path/advanced-react/src/context/AuthContext.jsx
@@ -1,12 +1,42 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import supabase from "../../utils/supabase";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  // Auth functions (signin, signup, logout)
-
   // Seesion state (user info, sign-status)
   const [session, setSession] = useState(undefined);
+
+  const getInitialSession = async () => {
+    try {
+      const { data, error } = await supabase.auth.getSession();
+      if (error) throw error;
+
+      console.log(data.session);
+      setSession(data.session);
+    } catch (error) {
+      console.error("Failed to get user session", error.message);
+    }
+  };
+
+  const handleAuthChange = (_event, session) => {
+    setSession(session);
+    console.log(`Session changed: ${session}`);
+  };
+
+  useEffect(() => {
+    // 1. Check on first render for a sessonion (getSession())
+
+    getInitialSession();
+
+    // 2. Listen for changes in auth state
+    const { data } = supabase.auth.onAuthStateChange(handleAuthChange);
+
+    // Clean up
+    return () => data.subscription.unsubscribe();
+  }, []);
+
+  // Auth functions (signin, signup, logout)
 
   return (
     <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
